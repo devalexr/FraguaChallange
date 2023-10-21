596f3a244dbdc783ae7b425c3ceded5059f9644f
Alejandro Robles <b_re3@hotmail.com>
2023-10-21 11:12:52 -0600

    search module

--- src/UI/screens/controllers/VControllerSearch.js
+++ src/UI/screens/controllers/VControllerSearch.js
@@ -2,14 +2,72 @@ import VModelSearch from '../models/VModelSearch';
 import VControllerPagination from './VControllerPagination';
 
 export default class VControllerSearch extends VControllerPagination {
+  static initialState = {
+    data: [],
+    loading: true,
+    loadingMore: false,
+    query: '',
+    showingSuggestions: true,
+  };
+
   static async getPaginationDATA() {
-    const data = await VModelSearch.search('dogs', this._page);
+    const query = this.view.state.query;
+    if (!query) {
+      return [];
+    }
+
+    const data = await VModelSearch.search(this.view.state.query, this._page);
     return data.results;
   }
 
+  static search() {
+    const query = this.view.state.query;
+    if (!query) {
+      return;
+    }
+    this.setState(
+      {
+        data: [],
+      },
+      () => {
+        this._page = 0;
+        this._paginate();
+      },
+    );
+  }
+
   //=============== VIEW EVENTS =============
+  static onSearch() {
+    this.search();
+  }
+
+  static onChageSearchQuery(query) {
+    this.setState({
+      query: query,
+    });
+  }
+
+  static onPressSearchSuggestion(query) {
+    this.setState(
+      {
+        query: query,
+        showingSuggestions: false,
+      },
+      () => {
+        this.search();
+      },
+    );
+  }
 
   static onPressItem(item) {
     console.log(item);
+  }
+
+  static onPressLikeButtom(itemId) {
+    alert('liked image: ' + itemId);
+  }
+
+  static onPressUser(userId) {
+    alert('go to user profile: ' + userId);
   }
 }
--- src/UI/screens/views/VIEWSearch.js
+++ src/UI/screens/views/VIEWSearch.js
@@ -1,8 +1,11 @@
-import React, {Component} from 'react';
-import {Text} from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {Pressable, Text, View} from 'react-native';
 import VIEWPagination from './VIEWPagination';
 import VControllerSearch from '../controllers/VControllerSearch';
 import {UNPhotoItem} from '../../components/views/UNPhotoItem';
+import AutocompleteInput from 'react-native-autocomplete-input';
+import {GUI_colors, GUI_styles} from '../../styles/STYLESMain';
 export default class VIEWSearch extends VIEWPagination {
   VController = VControllerSearch;
 
@@ -10,6 +13,14 @@ export default class VIEWSearch extends VIEWPagination {
     super(props);
     this.VController.setView(this);
   }
+
+  componentDidMount() {
+    //super.componentDidMount();
+    //this.searchInput.focus();
+    console.log(this.searchInput);
+  }
+
+  //======================== RENDER =================
 
   renderItem(item) {
     return (
@@ -25,6 +36,89 @@ export default class VIEWSearch extends VIEWPagination {
           this.VController.onPressLikeButtom(itemId);
         }}
       />
+    );
+  }
+
+  renderSearchSuggestion(item) {
+    return (
+      <Pressable
+        onPress={() => {
+          this.VController.onPressSearchSuggestion(item.value);
+        }}
+        style={{
+          justifyContent: 'center',
+          paddingHorizontal: 10,
+          paddingVertical: 14,
+          borderBottomWidth: 1,
+          borderBottomColor: GUI_colors.COLOR_BORDER,
+        }}>
+        <Text style={[GUI_styles.fontPM, {}]}>{item.value}</Text>
+      </Pressable>
+    );
+  }
+
+  renderHeader() {
+    return (
+      <View
+        style={{
+          zIndex: 10,
+          borderTopColor: GUI_colors.COLOR_BORDER,
+          borderTopWidth: 1,
+          marginBottom: 80,
+        }}>
+        <View
+          style={{
+            flex: 1,
+            left: 0,
+            position: 'absolute',
+            right: 0,
+            top: 0,
+            zIndex: 1,
+          }}>
+          <AutocompleteInput
+            autoFocus={true}
+            returnKeyType="search"
+            hideResults={!this.state.showingSuggestions}
+            placeholder="Buscar imágenes"
+            containerStyle={{
+              backgroundColor: 'white',
+              marginHorizontal: -2,
+            }}
+            style={[
+              {
+                height: 50,
+                backgroundColor: GUI_colors.COLOR_BACKGROUND_GRAY,
+                borderRadius: 30,
+                paddingLeft: 12,
+              },
+              GUI_styles.fontBold,
+            ]}
+            inputContainerStyle={{
+              borderRadius: 30,
+              margin: 10,
+            }}
+            data={[
+              {
+                value: 'Dogs',
+                created: 102354,
+              },
+            ]}
+            value={this.state.query}
+            onChangeText={query => {
+              this.VController.onChageSearchQuery(query);
+            }}
+            onSubmitEditing={() => {
+              this.VController.onSearch();
+            }}
+            flatListProps={{
+              keyExtractor: (_, idx) => idx,
+              renderItem: ({item}) => {
+                return this.renderSearchSuggestion(item);
+              },
+            }}
+          />
+        </View>
+      </View>
     );
   }
 }
